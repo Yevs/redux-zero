03adf084b12d61330cac623349deefdea66fcafc
Matheus Lima <matheusml90@gmail.com>
2017-10-17 21:21:44 -0200

    First (incomplete) version

--- rollup.config.js
+++ rollup.config.js
@@ -1,16 +1,17 @@
-import typescript from 'rollup-plugin-typescript2';
-import resolve from 'rollup-plugin-node-resolve';
-import commonjs from 'rollup-plugin-commonjs';
-import uglify from 'rollup-plugin-uglify';
-import filesize from 'rollup-plugin-filesize';
-import peerDeps from 'rollup-plugin-peer-deps-external';
+import typescript from 'rollup-plugin-typescript2'
+import resolve from 'rollup-plugin-node-resolve'
+import commonjs from 'rollup-plugin-commonjs'
+import uglify from 'rollup-plugin-uglify'
+import filesize from 'rollup-plugin-filesize'
+import peerDeps from 'rollup-plugin-peer-deps-external'
+import multiEntry from 'rollup-plugin-multi-entry'
 
-const format = process.env.NODE_ENV;
-const isUmd = format === 'umd';
+const format = process.env.NODE_ENV
+const isUmd = format === 'umd'
 const file = `dist/redux-zero.${isUmd ? 'min' : format}.js`
 
 const config = {
-  input: './src/index.ts',
+  input: ['./src/index.ts', './src/react/index.ts'],
   name: 'redux-zero',
   sourcemap: true,
   output: {
@@ -26,9 +27,10 @@ const config = {
       browser: true,
     }),
     commonjs(),
+    multiEntry(),
   ],
-};
+}
 
-isUmd && config.plugins.push(uglify(), filesize());
+isUmd && config.plugins.push(uglify(), filesize())
 
-export default config;
+export default config
